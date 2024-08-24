5b50b7ce3fd85bb3493eb53ae7b01cf423283f7f
Nanda Kishore Jana <nkcreatorjana32541@gmail.com>
2024-08-24 08:00:21 +0530

    Create Question1.py

---
+++ Question1.py
@@ -0,0 +1,7 @@
+#Q1. Count number of words in a string
+
+String_words = input("Enter the sentence: ")
+print("The Original String is: " + String_words)
+res = (len(String_words.split()))
+print("The number of string words in sentence: " + str(res))
+print("The number of string letter in sentence:",len(String_words))
